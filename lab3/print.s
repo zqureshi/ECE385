d251e10a9909310699b506d6a7fe25197991975d
Zeeshan Qureshi <zee@zqureshi.in>
2012-10-01 00:13:56 -0400

    Started Lab 3.

---
+++ lab3/print.s
@@ -0,0 +1,11 @@
+/*
+ * ECE385: Lab 3
+ * Zeeshan Qureshi (zeeshan.qureshi@mail.utoronto.ca)
+ */
+
+.text
+.global printn
+
+printn:
+  /* Placeholder for function so that we can compile properly */
+  ret
