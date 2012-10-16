4cc7dabbb82c66f5dee2d5901895841eec19585d
Zeeshan Qureshi <zee@zqureshi.in>
2012-10-15 22:50:54 -0400

    Added random number generation and game end logic.

--- lab5/numbers_game.s
+++ lab5/numbers_game.s
@@ -5,7 +5,35 @@
  */
 
 .equ ADDR_UART, 0x10001000   /* Address of JTAG UART Port */
+.equ ADDR_TIMER, 0x10002000  /* Timer device */
+.equ WAIT_CYCLES, 50000000   /* Number of timer cycles to wait */
+.equ FLASH_CYCLES, 37500000  /* Number of cycles to wait when flashing */
 .equ SPACE, 0x20             /* ASCII space character */
+
+/* MOVIA replacement */
+.macro MOVI32 reg, val
+  movhi \reg, %hi(\val)
+  ori \reg, \reg, %lo(\val)
+.endm
+
+/* Call output_string with the address of the string provided */
+.macro print string
+movi32 r4, \string
+call output_string
+.endm
+
+/* Call shift_buf_left with the string and replacement character */
+.macro sbl string replacement
+movi32 r4, \string
+mov r5, \replacement
+call shift_buf_left
+.endm
+
+/* Wait for given cycles by polling timer */
+.macro wait cycles
+movi32 r4, \cycles
+call timer_countdown
+.endm
 
 .data
 
@@ -16,22 +44,11 @@ BUFFER:
 RESET_TERM:                  /* Control sequences to reset terminal */
 .string "\x1b[2J\x1b[H"
 
-/*
- * Macro to call output_string with the address of the string provided
- */
-.macro print string
-movia r4, \string
-call output_string
-.endm
+RESET_CURSOR:                /* Reset cursor to home position */
+.string "\x1b[H"
 
-/*
- * Macro to call shift_buf_left with the string and replacement character
- */
-.macro sbl string replacement
-movia r4, \string
-movi r5, \replacement
-call shift_buf_left
-.endm
+OVER_MESSAGE:
+.string "GAME OVER!"          /* Message to display when game is over */
 
 .text
 .global main
@@ -39,15 +56,36 @@ call shift_buf_left
 /*
  * Register Allocation
  * r8: Address of JTAG UART (Shared by all other methods)
+ * r9: Address of buffer
+ * r10: Current character at buffer
+ * r15: Temp / Comparison Value
  */
 main:
-movia r8, ADDR_UART
+movi32 r8, ADDR_UART
+movi32 r9, BUFFER
 
 loop:
+call lab5_rand               /* Generate random character */
+
+ldb r10, 0(r9)               /* If first character in buffer is not a space */
+cmpeqi r16, r10, SPACE       /* then game is over */
+beq r0, r16, game_over
+
+sbl BUFFER r2                /* Insert generated character and display */
 print RESET_TERM
 print BUFFER
-sbl BUFFER, 0x31
+print RESET_CURSOR
+
+wait WAIT_CYCLES
 br loop
+
+game_over:
+print RESET_TERM
+wait FLASH_CYCLES
+print OVER_MESSAGE
+print RESET_CURSOR
+wait FLASH_CYCLES
+br game_over
 
 /*
  * Write buffer to UART
@@ -96,4 +134,36 @@ sbl_ins_rep:
 stb r5, 0(r4)                /* Insert replacement char at the position */
 
 sbl_exit:
+ret
+
+/*
+ * Start Timer to run for WAIT_CYCLES
+ *
+ * Register Allocation
+ * r4: Argemunt: Number of cycles to wait
+ * r5: Address of Timer Device
+ * r6: Value read from / write to Timer
+ */
+timer_countdown:
+movi32 r5, ADDR_TIMER
+
+movi32 r6, 0x0                        /* Clear Timer */
+stwio r6, 0(r5)
+
+mov r6, r4                            /* Write lower half of period */
+andi r6, r6, 0x0000ffff
+stwio r6, 8(r5)
+
+mov r6, r4                            /* Write upper half of period */
+srli r6, r6, 16
+stwio r6, 12(r5)
+
+movi32 r6, 0x4                        /* Start Timer */
+stwio r6, 4(r5)
+
+timer_wait:
+ldwio r6, 0(r5)                      /* Check if timer has timed out */
+andi r6, r6, 0x1
+beq r0, r6, timer_wait
+
 ret
