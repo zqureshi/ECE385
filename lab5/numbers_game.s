d0525e0fe068a7bf2eebbd0c2c0f581636772ff4
Zeeshan Qureshi <zee@zqureshi.in>
2012-10-15 21:58:31 -0400

    Finished buffer shifting subrouting and macro.

--- lab5/numbers_game.s
+++ lab5/numbers_game.s
@@ -5,11 +5,12 @@
  */
 
 .equ ADDR_UART, 0x10001000   /* Address of JTAG UART Port */
-.equ BUFFER_LEN, 40          /* Length of output buffer */
 .equ SPACE, 0x20             /* ASCII space character */
 
+.data
+
 BUFFER:
-.skip 40, SPACE              /* Standard null-terminated array */
+.skip 10, SPACE              /* Standard null-terminated array */
 .byte 0x00
 
 RESET_TERM:                  /* Control sequences to reset terminal */
@@ -23,6 +24,15 @@ movia r4, \string
 call output_string
 .endm
 
+/*
+ * Macro to call shift_buf_left with the string and replacement character
+ */
+.macro sbl string replacement
+movia r4, \string
+movi r5, \replacement
+call shift_buf_left
+.endm
+
 .text
 .global main
 
@@ -33,9 +43,11 @@ call output_string
 main:
 movia r8, ADDR_UART
 
+loop:
 print RESET_TERM
 print BUFFER
-br main
+sbl BUFFER, 0x31
+br loop
 
 /*
  * Write buffer to UART
@@ -58,4 +70,30 @@ addi r4, r4, 1               /* Move to next character */
 br output_string
 
 os_exit:
+ret
+
+/*
+ * Shift each character in the buffer 1 position to the left, discarding
+ * the leftmost character and inserting the replacement char at the end.
+ *
+ * Register Allocation
+ * r4: Argument: Address of buffer
+ * r5: Argument: Replacement character to insert at end
+ * r6: Current character in buffer
+ */
+shift_buf_left:
+ldb r6, 0(r4)                /* If buffer is empty, then exit */
+beq r0, r6, sbl_exit
+
+sbl_loop:
+ldb r6, 1(r4)                /* Read next character and put in curr pos */
+beq r0, r6, sbl_ins_rep
+stb r6, 0(r4)
+addi r4, r4, 1
+br sbl_loop
+
+sbl_ins_rep:
+stb r5, 0(r4)                /* Insert replacement char at the position */
+
+sbl_exit:
 ret
